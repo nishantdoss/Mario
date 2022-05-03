5ab89aa394e3b2daa97443bd06b6307cb5a9cfd8
unknown <mn9990@pleasantonusd.net>
2022-05-03 10:20:54 -0700

    khang

--- src/Mario.java
+++ src/Mario.java
@@ -1,5 +1,17 @@
-public class Mario {
+import java.util.*;
+import java.awt.*;
 
+public class Mario {
+    private int x; //x coord
+    private int y; // y coord
+    private boolean right;
+    //also add dimensions of mario as a final variable
+    public Mario(int x, int y, boolean right) {
+        this.x = x;
+        this.y = y;
+        this.right = right;
+
+    }
     public static void Move() {
         
     }
