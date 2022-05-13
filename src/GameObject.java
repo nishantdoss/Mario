59247a339682840f8ffc8dbfc8c0b27cd5d72813
mkhangnguyen <mn9990@pleasantonusd.net>
2022-05-13 09:15:54 -0700

    fjkds

--- src/GameObject.java
+++ src/GameObject.java
@@ -1,6 +1,10 @@
-public class GameObject {
+import java.io.File;
+
+public class GameObject extends HitBox {
     private int x;
     private int y;
-    
+    public GameObject(int x, int y, File f) {
+        super(x, y, f);
+    }
     
 }
