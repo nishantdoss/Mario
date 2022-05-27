4f53e86e7de6bd6da91706b32173db9bad548046
nishantdoss <nishantdoss@gmail.com>
2022-05-27 09:25:48 -0700

    name of game over

--- src/Background.java
+++ src/Background.java
@@ -10,7 +10,8 @@ import javax.swing.*;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 public class Background {
-private Image back;
+    private Image back;
+    
     public Background() {
 
     }
